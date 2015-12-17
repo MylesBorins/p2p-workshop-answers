86a370d8f5f7ff09a3c8a8209d28157e03763554
Myles Borins <mborins@us.ibm.com>
2015-12-17 15:42:29 -0800

    dun numba 3

--- client.js
+++ client.js
@@ -1,10 +1,18 @@
 var net = require('net');
-var socket = net.connect(10000, 'localhost');
+var jsonStream = require('duplex-json-stream');
+
+var username = process.argv[2] || 'anonymous';
+
+var socket = jsonStream(net.connect(10000, 'localhost'));
 
 process.stdin.on('data', function (data) {
-  socket.write(data);
+  var message = data.toString().trim()
+  socket.write({
+    username: username,
+    message: message
+  });
 });
 
 socket.on('data', function (data) {
-  process.stdout.write(data);
+  process.stdout.write(`${data.username}> ${data.message}`);
 });
--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 var net = require('net');
 
+var streamSet = require('stream-set');
+
+var activeSockets = streamSet();
+
 var server = net.createServer(function (socket) {
+  activeSockets.add(socket);
   console.log('new connection');
   socket.on('data', function (data) {
-    socket.write(data);
+    activeSockets.forEach(function (sock) {
+      if (sock !== socket) {
+        sock.write(data);
+      }
+    });
   });
 });
 
